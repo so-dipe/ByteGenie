a3394fd95bda4f5dd466c40733f6395c0a2ac533
sodipe <98396909+so-dipe@users.noreply.github.com>
2024-01-02 04:47:42 +0000

    bring back render.

--- app/__init__.py
+++ app/__init__.py
@@ -6,19 +6,19 @@ CORS(app)
 
 from app.routes import qa, summary, topics
 
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# @app.route("/question_answering")
-# def render_question_answering():
-#     return render_template("nlp_apps/question_answering.html")
+@app.route("/question_answering")
+def render_question_answering():
+    return render_template("nlp_apps/question_answering.html")
 
-# @app.route("/document_summarizing")
-# def render_document_summarising():
-#     return render_template("nlp_apps/document_summarizer.html")
+@app.route("/document_summarizing")
+def render_document_summarising():
+    return render_template("nlp_apps/document_summarizer.html")
 
-# @app.route("/topics")
-# def render_topics_extractor():
-#     return render_template("nlp_apps/topics.html")
+@app.route("/topics")
+def render_topics_extractor():
+    return render_template("nlp_apps/topics.html")
 
